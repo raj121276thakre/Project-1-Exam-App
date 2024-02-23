ff0877f53d07f5a78f37c939bb40f6a16b54c285
raj121276thakre <raj121276thakre@gmail.com>
2024-02-23 19:56:37 +0530

    3) signup & signin done using email and password

--- app/src/main/java/com/app/examapp/authentication/SignInActivity.kt
+++ app/src/main/java/com/app/examapp/authentication/SignInActivity.kt
@@ -6,31 +6,54 @@ import android.os.Bundle
 import android.widget.Toast
 import com.app.examapp.MainActivity
 import com.app.examapp.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*after clicking on the text "Not Registered Yet , Sign Up !"
-         the signup page will display */
-        binding.textGotoSignUp.setOnClickListener{
-            val intent = Intent(this, SignUpActivity::class.java )
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textGotoSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
-            finish() // Finish SignInActivity to remove it from back stack
         }
 
-        //after clicking on the "SignIn" button the below code will execute
         binding.btnSignIn.setOnClickListener {
-            Toast.makeText(this,"SignIn button will be implemented soon",Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity::class.java )
-            startActivity(intent)
-            finish()
-        }
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
 
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/app/examapp/authentication/SignUpActivity.kt
+++ app/src/main/java/com/app/examapp/authentication/SignUpActivity.kt
@@ -4,32 +4,62 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.app.examapp.MainActivity
 import com.app.examapp.databinding.ActivitySignUpBinding
-
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignUpBinding  // binding declaration
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater) // binding declaration
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*after clicking on the text "Already Registered , Sign In !"
-         the signin page will display */
-        binding.textGotoSignin.setOnClickListener{
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Check if the user is already signed in
+        if (firebaseAuth.currentUser != null) {
+            // User is already signed in, open MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Finish SignUpActivity to prevent going back to it when pressing back button
+        }
+
+        binding.textGotoSignin.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
-            finish() // Finish SignUpActivity to remove it from back stack
         }
-
-        //after clicking on the "Sign up" button the below code will execute
         binding.btnSignUp.setOnClickListener {
-            Toast.makeText(this,"SignUp button will be implemented soon", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, SignInActivity::class.java )
-            startActivity(intent)
-            finish()
-        }
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
 
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            // Check again if the user is signed in after sign-up
+                            if (firebaseAuth.currentUser != null) {
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish() // Finish SignUpActivity to prevent going back to it when pressing back button
+                            } else {
+                                Toast.makeText(this, "User authentication failed.", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
+
+
